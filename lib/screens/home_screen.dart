ff1cd9aed6ac5bde9d8d9a3aa450097792ed11df
진수민 <jimmy@jinsumin-ui-MacBookPro.local>
2024-05-23 10:23:46 +0900

    pomodoros app completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,14 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
   bool isRunning = false;
+  int totalPomodoros = 0;
   late Timer timer;
 
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds = totalSeconds - 1;
-    });
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
   }
 
   void onStartPressed() {
@@ -34,6 +45,12 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  String formatTimer(int seconds) {
+    var duration = Duration(seconds: seconds);
+    // print(duration.toString().split("."));
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                 alignment: Alignment.bottomCenter,
                 child: Text(
-                  '$totalSeconds',
+                  formatTimer(totalSeconds),
                   style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
@@ -88,7 +105,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                         .displayLarge!
                                         .color,
                                   )),
-                              Text('0',
+                              Text('$totalPomodoros',
                                   style: TextStyle(
                                     fontSize: 58,
                                     fontWeight: FontWeight.bold,
